44fb4ae79495f7b9a162c23f055e24fd76535abe
Ankit Gupta <ankitgupta92368892@gmail.com>
2024-07-12 01:52:38 +0530

    3q update 2

--- config/conn.js
+++ config/conn.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// console.log(process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected successfully");
   } catch (err) {
-    console.error(err.message);
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1); // Exit process with failure
   }
 };
 
