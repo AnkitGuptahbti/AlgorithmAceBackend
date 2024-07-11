3bd24d5a8ceb7f54b69c6c772143f723b729d4d8
Ankit Gupta <ankitgupta92368892@gmail.com>
2024-07-12 01:26:20 +0530

    3q update

--- routes/testcases.js
+++ routes/testcases.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const TestCase = require("../models/Testcase");
+const TestCase = require("../models/TestCase");
 // Add a new test case
 router.post("/", async (req, res) => {
   try {
